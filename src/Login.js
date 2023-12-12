b21e9f7fd65a15dff861aa8cd1a254a0b4a81a22
shivishivendra123 <abhishivitony@rediffmail.com>
2023-12-12 16:52:44 +0530

    Sign In and Sign Up form

---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import Header from "./Header"
+
+const Login = ()=>{
+    const [loginForm,setLoginForm] = useState(true);
+    return (
+        <div>
+            <Header/>
+            <img className="absolute" src="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="logo"/>
+            <form className="bg-black absolute w-4/12 my-24 p-10 mx-auto right-0 left-0 text-white bg-opacity-80">
+                <h1 className="font-bold text-3xl mb-2"> {loginForm?"Sign In":"Sign Up"}</h1>
+                {!loginForm?<input type="text" placeholder="Type your full name..." className="p-2 my-4 w-full bg-slate-500  font-bold"/>:null}
+                <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-slate-500  font-bold"/>
+                <input type="password" placeholder="Enter Password" className="p-2 my-4 w-full bg-slate-500 font-bold"/>
+                {!loginForm?<input type="password" placeholder="Confirm Password" className="p-2 my-4 w-full bg-slate-500 font-bold"/>:null} 
+                <button className="bg-[#E50914] w-full my-4 h-[50px] rounded-md" type="submit">{loginForm?"Log In":"Sign Up"}</button> 
+                {loginForm?<p className="font-bold">New to Netflix? Sign Up Now<button className="bg-[#E50914] mx-10 w-[100px] rounded-sm" onClick={()=>{
+                    var lg = loginForm;
+                    setLoginForm(!lg)
+                }} type="button">Sign Up</button></p>:null}
+                {!loginForm?<p className="font-bold">Already have an Account<button className="bg-[#E50914] mx-10 w-[100px] rounded-sm" onClick={()=>{
+                    var lg = loginForm;
+                    setLoginForm(!lg)
+                }} type="button">Sign In</button></p>:null}
+            </form>
+        </div>
+    )
+}
+
+export default Login
